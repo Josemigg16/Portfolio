051da73d0d06e96ab7439dfdcfdc16d5a32c4daf
Jose Miguel <josemiggomez16@gmail.com>
2024-04-07 22:05:48 -0400

    add prettier and eslint

---
+++ .prettierrc.cjs
@@ -0,0 +1,20 @@
+const { rules } = require("eslint-config-prettier");
+
+module.exports = {
+	printWidth: 100,
+	semi: false,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'none',
+	useTabs: true,
+	overrides: [
+		{
+			files: ['*.json', '*.md', '*.toml', '*.yml'],
+			options: {
+				useTabs: false
+			}
+		}
+	],
+	endOfLine: 'lf',
+}
