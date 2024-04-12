65d4466ca094f09b41c9398164e372ff5a5ea975
josemiggomez16 <josemiggomez16@gmail.com>
2024-04-12 03:12:28 +0000

    create util to getIcons for a proyect

---
+++ src/utils/iconList.ts
@@ -0,0 +1,23 @@
+import AstroIcon from '../components/svg/tech/AstroIcon.astro'
+import ExpressjsIcon from '../components/svg/tech/ExpressjsIcon.astro'
+import NextIcon from '../components/svg/tech/NextIcon.astro'
+import NodeIcon from '../components/svg/tech/NodeIcon.astro'
+import PostgresIcon from '../components/svg/tech/PostgresIcon.astro'
+import PrismaIcon from '../components/svg/tech/PrismaIcon.astro'
+import ReactIcon from '../components/svg/tech/ReactIcon.astro'
+import TailwindIcon from '../components/svg/tech/TailwindIcon.astro'
+
+export const getIcons = (selectedIcons: string[]) => {
+	const iconList = []
+	selectedIcons.forEach((icon) => {
+		if (icon === 'astro') iconList.push({ Icon: AstroIcon, className: 'mt-2.5 h-10 w-10' })
+		if (icon === 'express') iconList.push({ Icon: ExpressjsIcon, className: 'mt-2 h-11 w-11' })
+		if (icon === 'next') iconList.push({ Icon: NextIcon, className: 'h-10 w-10' })
+		if (icon === 'node') iconList.push({ Icon: NodeIcon, className: 'h-10 w-10' })
+		if (icon === 'postgres') iconList.push({ Icon: PostgresIcon, className: 'mt-1.5 h-10 w-10' })
+		if (icon === 'prisma') iconList.push({ Icon: PrismaIcon, className: 'h-9 w-9' })
+		if (icon === 'react') iconList.push({ Icon: ReactIcon, className: 'h-10 w-10' })
+		if (icon === 'tailwind') iconList.push({ Icon: TailwindIcon, className: 'h-10 w-10' })
+	})
+	return iconList
+}
