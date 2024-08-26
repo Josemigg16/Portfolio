9f040f35bfc02c6d553068fd0fe91a8eb168fe51
Jose Miguel <josemiggomez16@gmail.com>
2024-08-26 13:59:01 -0400

    finish v-1.1

---
+++ src/components/preact/TagPosts.tsx
@@ -0,0 +1,34 @@
+
+
+export default function Posts({ allPosts }: any) {
+	const [posts, setPosts] = useState(allPosts)
+
+
+	return posts.length === 0 ? (
+		<p class='mt-40 grid place-content-center'>No posts found</p>
+	) : (
+		<ul class='my-8 flex flex-wrap gap-10 pl-4'>
+			{posts.map((post: any) => (
+				<li class='mx-auto flex h-60 max-w-[250px] flex-col justify-between sm:mx-0'>
+					<picture class='grid h-full place-content-center rounded'>
+						<a href={`posts/${post.slug}`}>
+							<img
+								style={`view-transition-name:${post.data.id};`}
+								class='rounded bg-black'
+								src={post.data.image.url}
+								alt=''
+								width='250'
+							/>
+						</a>
+					</picture>
+					<a
+						class='line-clamp-3 px-2 pb-6 indent-2 font-semibold hover:underline'
+						href={`posts/${post.slug}`}
+					>
+						{post.data.title}
+					</a>
+				</li>
+			))}
+		</ul>
+	)
+}
