a3a7273a0cd7940736580fa648a48b08e0f2a999
Jose Miguel <josemiggomez16@gmail.com>
2024-09-19 23:29:16 -0400

    rss for collections

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,19 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
 
 export async function GET(context) {
-  return rss({
-    title: 'Josemigg Web',
-    description: 'This my personal portfolio and blog',
-    site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-us</language>`,
-  });
+	const posts = getCollection('posts')
+	return rss({
+		title: 'Josemigg Web',
+		description: 'This my personal portfolio and blog',
+		site: context.site,
+		items: await posts.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.pubDate,
+			description: post.data.description,
+			link: `/posts/${post.slug}/`
+		})),
+
+		customData: `<language>en-us</language>`
+	})
 }
